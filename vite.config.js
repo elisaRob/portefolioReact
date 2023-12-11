f61ea7d1d896f0fd6e3229297c1aead031c00dc1
Elisa <elisabethrobertaoucher@gmail.com>
2023-12-11 18:51:02 +0100

    ajouts

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,6 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Assurez-vous que le chemin du point d'entrée est correct
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    // Spécifiez le chemin de sortie (si nécessaire)
-    outDir: 'dist',
-  },
+  
  
 })
