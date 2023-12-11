42634d18706c5f20ddf4b8aa22d7f2c15f24afe8
Elisa <elisabethrobertaoucher@gmail.com>
2023-12-11 17:18:56 +0100

    ajouts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,14 @@
+import "../styles/home.css"
+
 const Home=()=>{
     return(
-        <></>
+       <div className="accueil">
+            <div className="quiSuisJe">
+                <span>Elisabeth ROBERT AOUCHER</span>
+                <h1>Développeuse Web</h1>    
+            </div>         
+            <img src="../public/media/img/montagne.jpg" alt=""/>
+       </div>
     )
 }
 
